12e0e5896d6c59a9fc09cf20447ffb4c743b8373
jrim <comfortindex@naver.com>
2022-07-05 02:00:06 +0900

    add: new struct -- t_tokcnt

--- src/minishell.h
+++ src/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:59:19 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/04 19:44:18 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/07/05 01:51:17 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,36 @@
 # include "../libft/libft.h"
 
 # define PRMPT "( ´Д`)> "
+# define QUOTE "'\""
+
+typedef struct s_tokcnt
+{
+	int	pipe;
+	int	rdr_in;
+	int	rdr_out;
+	int	rdr_app;
+	int	sq_cnt;
+	int	dq_cnt;
+} 		t_tokcnt;
 
 // parser가 executor에게 주는 선물은 다음과 같습니다...
-typedef struct s_parsed
+typedef struct s_token
 {
-	t_chunk			node;	//	각 노드는 |를 기준으로 나누어져있습니다.
-	struct s_parsed	*next;
-} 	t_parsed;
-
-typedef struct s_chunk
-{
-	int		in_rdr;		//	< 유무
+	int		rdr_in;		//	< 유무
 	char	**infile;	//	infile name
 	char	*cmd;		//	cmd일 것으로 예상되는 str
 	char	**param;	//	cmd 뒤에 오는 str들의 배열
-	int		out_rdr;	//	> 유무
+	int		rdr_out;	//	> 유무
 	char	**outfile;	//	outfile name
-	int		app_rdr;	//	>> 유무
+	int		rdr_app;	//	>> 유무
 	char	**appfile;	//	append 되어야할 file name
-} 	t_chunk;
+} 	t_token;
+
+typedef struct s_toklst
+{
+	t_token			node;	//	각 노드는 |를 기준으로 나누어져있습니다.
+	struct s_tklst	*next;
+} 	t_toklst;
 // char **의 경우 배열의 마지막은 NULL (ft_split처럼)
 // 여기까지가 parser로부터의 선물입니다.
 // thank you from executor
@@ -54,9 +65,19 @@ void	h_sigint(int signum);
 void	h_sigquit(int signum);
 
 // _check.c
+void	checker_main(char *line, t_tokcnt *checker);
 int		check_quote(char *line);
-int		check_pipe(char **cmd_token);
+int		check_pipe(char *line);
 int		check_RDR(char *line);
+
+// _parser.c
+void	parser_main(char *line, t_toklst *toklst);
+
+// _token.c
+void	init_token(t_token *tok);
+void	tok_rdr(t_token *tok, int rdr_flag, char *content);
+void	tok_cmd(t_token *tok, int cmd_flag, char *content);
+void	add_token(t_toklst *toklst, t_token *tok);
 
 // _utils.c
 void	free_all(char **str);
