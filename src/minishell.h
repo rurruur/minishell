5eb3d4971b62d219049f630a58f6792718a47cb3
jrim <comfortindex@naver.com>
2022-07-04 19:00:16 +0900

    구조체 내용 추가

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:59:19 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/04 00:07:33 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/04 18:58:04 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,26 @@
 
 # define PRMPT "( ´Д`)> "
 
-typedef struct s_cmd
+// parser가 executor에게 주는 선물은 다음과 같습니다...
+typedef struct s_parsed
 {
-	
-} t_cmd;
+	t_chunk			node;	//	각 노드는 |를 기준으로 나누어져있습니다.
+	struct s_parsed	*next;
+} 	t_parsed;
+
+typedef struct s_chunk
+{
+	int		in_rdr;		//	< 유무
+	char	**infile;	//	infile name
+	char	*cmd;		//	cmd일 것으로 예상되는 str
+	char	**param;	//	cmd 뒤에 오는 str들의 배열
+	int		out_rdr;	//	> 유무
+	char	**outfile;	//	outfile name
+	int		app_rdr;	//	>> 유무
+	char	**appfile;	//	append 되어야할 file name
+} 	t_chunk;
+// char **의 경우 배열의 마지막은 NULL (ft_split처럼)
+// 여기까지가 parser로부터의 선물입니다.
 
 // minishell.c
 
