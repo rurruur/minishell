72aaf95072b0351db04c317e0894b075c7a92f8b
jrim <comfortindex@naver.com>
2022-07-06 14:30:13 +0900

    구조체 수정

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:59:19 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/06 01:44:02 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/06 14:29:22 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,22 @@
 # define QUOTE "'\""
 # define RDR "<>"
 
-typedef struct s_strlst
-{
-	char			*str;
-	struct s_strlst *next;
-} 	t_strlst;
-
 // parser가 executor에게 주는 선물은 다음과 같습니다...
+typedef struct s_toklst
+{
+	t_token			*cmd;		//	cmd와 뒤에 오는 str들
+	t_token			*infile;	//	< : infile name
+	t_token			*outfile;	//	> : outfile name
+	t_token			*here_doc;	//	<< : here_doc에 들어가는 str들
+	t_token			*append;	//	>> : append 되어야할 file name
+	struct s_toklst	*next;
+} 					t_toklst;
+
 typedef struct s_token
 {
-	char		*cmd;		//	cmd일 것으로 예상되는 str
-	t_strlst	*param;	//	cmd 뒤에 오는 str들의 배열
-	int			rdr_in;		//	< 유무
-	t_strlst	**infile;	//	infile name
-	int			rdr_out;	//	> 유무
-	t_strlst	**outfile;	//	outfile name
-	int			here_doc;	//	>> 유무
-	t_strlst	**hd_str;	//	here_doc에 들어가는 str들
-	int			rdr_app;	//	>> 유무
-	t_strlst	**appfile;	//	append 되어야할 file name
-} 				t_token;
-
-typedef struct s_toknd
-{
-	t_token			*content;	//	각 노드는 |를 기준으로 나누어져있습니다.
-	struct s_tkoknd	*next;
-} 	t_toknd;
+	char			*str;
+	struct s_token	*next;
+} 					t_token;
 // char **의 경우 배열의 마지막은 NULL (ft_split처럼)
 // 여기까지가 parser로부터의 선물입니다.
 // thank you from executor
