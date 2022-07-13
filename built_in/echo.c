d12f9a382f6dfe9ef6cebaa4bd16823cdf20b378
jrim <comfortindex@naver.com>
2022-07-13 21:16:00 +0900

    built_in main 구현

--- built_in/echo.c
+++ built_in/echo.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:36:25 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/13 19:06:44 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/13 20:10:33 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	msh_echo(char **argv)
 
 	idx = 1;
 	n_opt = 0;
+	// -n option check를 먼저 해야할까?
 	if (argv[idx])	// list->size로 할까?
 	{
-		// -n option check를 먼저 해야할까?
 		// quote에 관한 처리들은 src/_token03.c에 있는 함수들 이용하면 될 듯
+		if (n_opt == 1)
+			printf("\n");
 	}
 }
