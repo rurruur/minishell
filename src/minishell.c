4040e778c6dff512a74b7dadfe9f4c8579aa01f2
nakkim <kdkeiie8@gmail.com>
2022-07-16 16:39:28 +0900

    executor: 파이프 관련.. 해결중

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:59:23 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/15 23:14:41 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/07/16 16:34:06 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@ int	main(int argc, char **argv, char **env)
 	while (argc)
 	{
 		line = readline(PRMPT);
-		dprintf(g_fd, "\n---readline---\n");
+		dprintf(g_fd, "\n---readline(%s)---\n", line);
 		if (line)
 		{
 			add_history(line);
 			toklst = tokenizer(line, NULL);
 			if (toklst)
 			{
-				display_toklst(toklst);
+				// display_toklst(toklst);
 				executor(toklst, env);
 			}
-			free_toklst(toklst);
-			// system("leaks minishell > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
+			// free_toklst(toklst);
+			// // system("leaks minishell > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
+			free(line);
 		}
 		else
 			break;
-		free(line);
 	}
 	close(g_fd);
 	return (0);
--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:59:19 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/15 23:15:26 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/07/16 16:06:51 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ typedef struct s_toklst
 	t_token			*heredoc;	//	<< : here_doc에 들어가는 str들
 	t_token			*rdr_out;
 	t_token			*trash;
+	int				end[2];
 	struct s_toklst	*next;
 } 					t_toklst;
 
@@ -126,11 +127,11 @@ void		display_strlst(t_token *strlst);
 
 // executor/redirection.c
 int			set_file_redirection(t_token *files, enum e_rdr mode);
-void		set_redirection(t_toklst *list, int *end);
+void		set_redirection(t_toklst *list, int end);
 
 // executor/executor.c
 int			is_builtin(char *cmd);
-void		child_process(t_toklst *list, int *end, char **env);
+void		child_process(t_toklst *list, int end, char **env);
 void		parent_process(pid_t child, int *end);
 void		executor(t_toklst *list, char **env);
 
