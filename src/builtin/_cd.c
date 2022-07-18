7f2b90ed0c9cf3bd85d89f9812eddf9cc70a9968
jisu rim <comfortindex@naver.com>
2022-07-18 17:44:14 +0900

    unset 수정, cd 수정 중

--- src/builtin/_cd.c
+++ src/builtin/_cd.c
@@ -6,13 +6,37 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:35:52 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/17 20:32:33 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/18 17:39:39 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 #define MAX_PATH_LEN 4096
+
+int		msh_cd(t_token *argv, t_env *envlst);
+void	_pwd_(void); // 나중에 지워라
+char	*_cd_get_path(t_token *argv, t_env *envlst);
+void	_cd_env(char *path, t_env *envlst);
+
+int	msh_cd(t_token *argv, t_env *envlst)
+{
+	char	*path;
+	
+	path = NULL;
+	if (argv->next == NULL)						// cd
+		path = get_env_val(envlst, "HOME");
+	else
+	{
+		argv = argv->next;
+		path = _cd_get_path(argv, envlst);
+	}
+	if (chdir(path) == -1)
+		printf("minishell: cd: %s: No such file or directory\n", path);
+	free(path);
+	// envlst 새로 setting ?
+	return (1);
+}
 
 void	_pwd_(void)
 {
@@ -23,13 +47,22 @@ void	_pwd_(void)
 	printf("pwd : %s\n", pwd);
 }
 
-void	_cd_home(char *path, t_env *envlst)
+char	*_cd_get_path(t_token *argv, t_env *envlst)
 {
-	path = get_env_val(envlst, "HOME");
-	printf("(home) path : %s\n", path);
-	if (chdir(path) == -1)
-		printf("HOME is not set\n");
-	_pwd_();
+	char	*path;
+	char	*str;
+	int		type;
+
+	path = NULL;
+	str = argv->str;
+	type = argv->type;
+	if (str[0] == '~' && str[1] == '\0')	// cd ~
+		path = get_env_val(envlst, "HOME");
+	else if (type == T_OFF)
+		path = ft_strdup(str);
+	else if (type == T_ENV)
+		;
+	return (path);
 }
 
 void	_cd_env(char *path, t_env *envlst)
@@ -43,31 +76,4 @@ void	_cd_env(char *path, t_env *envlst)
 	printf("(env) path : %s\n", path);
 	if (chdir(path) == -1)
 		chdir(get_env_val(envlst, "HOME"));
-}
-
-// void	_cd_get_path(char *path)
-// {
-	
-// }
-
-int	msh_cd(t_token *argv, t_env *envlst)
-{
-	char	*path;
-	
-	path = NULL;
-	if (argv->next)
-		path = argv->next->str;
-	if (argv->next == NULL)						// 그냥 cd
-		_cd_home(path, envlst);
-	else if (path[0] == '~' && path[1] == '\0')	// cd ~
-		_cd_home(path, envlst);
-	else										// cd 어쩌구
-	{
-		// type 확인해서 환경변수면 앞에서부터 올바른 경로만 잘라?
-		if (chdir(path) == -1)
-			printf("minishell: cd: %s: No such file or directory\n", path); // 임시 error msg
-		// envlst 새로 setting
-		_pwd_();
-	}
-	return (1);
 }
