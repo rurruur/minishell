18d5e336524283a982d3b7c1698d78a931dfb2f6
jrim <comfortindex@naver.com>
2022-07-22 17:25:38 +0900

    exit status

--- src/builtin/_exit.c
+++ src/builtin/_exit.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:36:39 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/21 22:53:40 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/22 17:24:57 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,30 @@
 int		msh_exit(t_token *argv, int type);
 int		_exit_num_check(char *str);
 void	_exit_err(int type, char *str);
+int		_exit_status(char *str);
 
 int		msh_exit(t_token *argv, int type)
 {
-	int		exit_num;
-
-	exit_num = 0;
 	if (type != EXIT_PIPE)
 		printf("exit\n");
 	if (argv->next == NULL)
-	{
 		g_status = 0;
-		return (1);
-	}
-	if (_exit_num_check(argv->next->str))
+	else if (_exit_num_check(argv->next->str) != 0)
 		_exit_err(2, argv->next->str);
 	else if (argv->next->next)
 		_exit_err(1, argv->next->str);
 	else
-	{
-		// exit_num = ft_atoi(argv->next->str);
-		// if (exit < 0)
-		// 	exit_num += 256;
-		// else
-		// 	exit_num %= 256;
-		(void)exit_num;
-		// -255~256
-		g_status = 0;
-	}
+		g_status = _exit_status(argv->next->str);
 	return (1);
 }
 
 int		_exit_num_check(char *str)
 {
 	int alpha;
+	int	len;
 
 	alpha = 0;
+	len = 0;
 	if (*str == '-')
 		str++;
 	while (*str && alpha == 0)
@@ -58,7 +46,10 @@ int		_exit_num_check(char *str)
 		if (!ft_isdigit(*str))
 			alpha = 1;
 		str++;
+		len++;
 	}
+	if (alpha == 1 && len > 19) // LLONG MAX
+		alpha = -1;
 	return (alpha);
 }
 
@@ -76,4 +67,36 @@ void	_exit_err(int type, char *str)
 		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 		g_status = 255;
 	}
+}
+
+// 9223372036854775807
+int		_exit_status(char *str)
+{
+	unsigned long long	num;
+	int					sign;
+	int					exit_status;
+
+	num = 0;
+	sign = 1;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (ft_isdigit(*str) == 1 && *str != '\0')
+	{
+		num += (*str) - '0';
+		if (ft_isdigit(*(str + 1)) == 1 && *(str + 1) != '\0')
+			num *= 10;
+		str++;
+	}
+	if ((sign == 1 && num > LLONG_MAX) ||\
+		(sign == -1 && num - 1 > LLONG_MAX))
+	{
+		_exit_err(2, str);
+		exit_status = 255;
+	}
+	else
+		exit_status = (sign * num) & 255;
+	return (exit_status);
 }
