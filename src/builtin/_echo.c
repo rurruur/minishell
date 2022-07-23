f23b341970186ab229e804271734e21944b216a2
jrim <comfortindex@naver.com>
2022-07-24 01:42:26 +0900

    echo 수정....

--- src/builtin/_echo.c
+++ src/builtin/_echo.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:36:25 by jrim              #+#    #+#             */
-/*   Updated: 2022/07/23 22:50:07 by jrim             ###   ########.fr       */
+/*   Updated: 2022/07/24 01:39:55 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ void	msh_echo(t_token *argv)
 	}
 	while (argv)
 	{
-		printf("%s", argv->str);
+		ft_putstr_fd(argv->str, STDOUT_FILENO);
 		argv = argv->next;
 		if (argv)
-			printf(" ");
+			ft_putstr_fd(" ", STDOUT_FILENO);
 	}
 	if (n_opt == 0)
-		printf("\n");
+		ft_putstr_fd("\n", STDOUT_FILENO);;
 }
 
 int	_echo_opt(char *str)
